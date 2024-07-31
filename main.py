a841a52092f11682f5e034ae5dec06745be8f785
Mazen Moazzam <mm5622@drexel.edu>
2024-07-30 21:14:48 -0400

    Working on Order Management class, implemented SMS notifications with users phone using Twilio API for place order, will implement for order update and cancellation of orders.

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ apiKey = 'PKG126Z4K4SABUBFSYBG'
 secretKey = '2KdUrhkh83ZTZc24AtmU1Lc79HlcaQtERLZhwDrE'
 url = 'https://paper-api.alpaca.markets'
 
+twilio_sid = 'ACd08d4a99a3a12582ad7d30f50c3dd575'
+twilio_auth_token = '7084ed704ff6dcc47d2d9e87330324d4'
+twilio_phone_number = '+18772741628'
+
 
 def main():
     alpacaApi = REST(apiKey, secretKey, url, api_version='v2')
     data = DataHandler(alpacaApi)
-    order = OrderManager(alpacaApi)
+    order = OrderManager(alpacaApi, twilio_sid, twilio_auth_token, twilio_phone_number)
 
     pd.set_option('display.max_columns', None)
     pd.set_option('display.expand_frame_repr', False)
@@ -27,6 +31,6 @@ def main():
     end_date = '2024-07-10'
     historical_data = data.get_historical_data(symbol, start_date, end_date)
     port = data.get_portfolio()
-    print(order.get_cash())
+    order.place_order('TSLA', 1, 'buy', 'market', 'gtc', '+14043885784')
 if __name__ == "__main__":
     main()
--- orderManagement.py
+++ orderManagement.py
@@ -1,12 +1,26 @@
 from alpaca_trade_api.rest import REST, TimeFrame
+from twilio.rest import Client
+
 
 class OrderManager:
-    def __init__(self, api):
+    def __init__(self, api, twilio_sid, twilio_auth_token, twilio_phone_number):
         self.api = api
+        self.twilio_client = Client(twilio_sid, twilio_auth_token)
+        self.twilio_phone_number = twilio_phone_number
 
+    def send_sms_notification(self, number, message):
+        try:
+            message = self.twilio_client.messages.create(
+                body=message,
+                from_=self.twilio_phone_number,
+                to=number
+            )
+            return message.sid
+        except Exception as e:
+            print(f'Error sending SMS notification: {e}')
+            return None
 
-
-    def place_order(self, symbol, qty, side, order_type, time_in_force):
+    def place_order(self, symbol, qty, side, order_type, time_in_force, notifyNumber = None):
         ''' Function used to place orders (buy or sell) on stocks
             Symbol is the ticker symbol of the stock, qty is simply the amount of shares,
             side (whether you want to buy or sell)
@@ -19,6 +33,11 @@ class OrderManager:
                 type=order_type,
                 time_in_force=time_in_force
             )
+            if notifyNumber:
+                self.send_sms_notification(
+                    notifyNumber,
+                    f'Order placed: {symbol} {side} {qty} shares at {order_type}'
+                )
             return order
         except Exception as e:
             print(f"Error placing order: {e}")
@@ -57,6 +76,64 @@ class OrderManager:
                 return None
         except Exception as e:
             print(f'Error getting cash amount: {e}')
+            return None
+
+    def get_positions(self):
+        try:
+            positions = self.api.list_positions()
+            return positions
+        except Exception as e:
+            print(f'Error getting positions: {e}')
+            return None
+
+    def update_order(self, order_id, qty=None, time_in_force=None):
+        try:
+            order = self.api.replace_order(
+                    order_id = order_id,
+                    qty=qty,
+                    time_in_force=time_in_force
+            )
+            return order
+        except Exception as e:
+            print(f'Error updating order: {e}')
+            return None
+
+    def retrieve_order_by_id(self,order_id):
+        try:
+            order = self.api.get_order(order_id)
+            return {
+                'id': order.id,
+                'symbol': order.symbol,
+                'qty': order.qty,
+                'side': order.side,
+                'type': order.type,
+                'status': order.status,
+                'created at': order.created_at,
+                'filled_at': order.filled_at
+            }
+        except Exception as e:
+            print(f'Error retrieving order by ID: {e}')
+            return None
+
+    def list_order_history(self, status='all'):
+        try:
+            orders = self.api.list_orders(status=status)
+            order_details = [
+                {
+                    'id': order.id,
+                    'symbol': order.symbol,
+                    'qty': order.qty,
+                    'side': order.side,
+                    'type': order.type,
+                    'status': order.status,
+                    'created_at': order.created_at,
+                    'filled at': order.filled_at
+                }
+                for order in orders
+            ]
+            return order_details
+        except Exception as e:
+            print(f'Error getting order details: {e}')
             return None
 
 
