40132a8b226e6fc7bd85133eaab28900075efd81
Mazen Moazzam <mm5622@drexel.edu>
2024-08-10 16:06:07 -0400

    Fixed errors for sentiment analysis scores, instead of accessing dictionary keys, used get() function to specify default value if the keyword wasn't found, preventing type errors.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
     order = OrderManager(alpacaApi, twilio_sid, twilio_auth_token, twilio_phone_number)
     portfolio = [('LMT', 1)]
     trading = TradingLogic(alpacaApi, data, order, portfolio)
-
+    print(trading.getSentimentScore('PENN'))
 
 if __name__ == "__main__":
     main()
