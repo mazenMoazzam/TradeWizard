62d4df69b7c15b51f24f6782a38fc78442f68cd9
mazenMoazzam <137253449+mazenMoazzam@users.noreply.github.com>
2024-08-10 21:37:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,6 @@ from dataHandler import DataHandler
 from orderManagement import OrderManager
 from tradingLogic import TradingLogic
 
-# apiKey = 'PKG126Z4K4SABUBFSYBG'
-# secretKey = '2KdUrhkh83ZTZc24AtmU1Lc79HlcaQtERLZhwDrE' to keep for reference.
-# url = 'https://paper-api.alpaca.markets'
-
-apiKey = 'PKG126Z4K4SABUBFSYBG'
-secretKey = '2KdUrhkh83ZTZc24AtmU1Lc79HlcaQtERLZhwDrE'
-url = 'https://paper-api.alpaca.markets'
-
-twilio_sid = 'AC8be78dea1f6134d36b5682bdf46e176e'
-twilio_auth_token = '19aa62b8a83555641e5f70a42e031b80'
-twilio_phone_number = '+18888713828'
-
 
 def main():
     alpacaApi = REST(apiKey, secretKey, url, api_version='v2')
